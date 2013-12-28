7b79394d47adab9fda5a752eafea30d31bb27d39
Steve Kondik <shade@chemlab.org>
2013-12-28 15:48:20 -0800

    build: Add QCOM target-specific config
     * Needed to support global DirectTract config on legacy targets
     * Let's also eliminate some boilerplate
    
    Change-Id: I736c10a5e7e1f3d1e0de9e60f29b60add276f151
    
    build: Fix cflags for QC targets
    
    Change-Id: I2281bec3afb4d80e80845718d880dc24ef7baf32
    
    build: qcom: Set QCOM_*_PATH variables for hardware/qcom-* projects
    
    This consolidates a bunch of one-off logic scattered throughout the
    code base.  Usage in Android.mk files is trivial:
    
    At top level, use e.g. "ifeq ($(call my-dir),$(QCOM_AUDIO_PATH))".
    This works for all variants, including non-variants (i.e. AOSP).
    
    Within subdirs, use e.g. hardware/qcom/audio => $(QCOM_AUDIO_PATH)
    
    Change-Id: Iee2497ea9a7efeb4ae9e861b84c532b19da7b69d
    
    build: Introduce project pathmap and use it for qcom variants
    
    The project pathmap tracks the path to the top level of a project.  The
    following functions are provided:
    
     * project-set-path adds an entry.
     * project-set-path-variant adds a "variant" entry.
     * project-path-for retrieves an entry.
    
    To use as a guard in Android.mk:
    
      ifeq ($(call my-dir),$(call project-path-for,projectname))
    
    To use for include paths in Android.mk:
    
      LOCAL_C_INCLUDES += $(call project-path-for,projectname)/...
    
    Set project pathmap for qcom project variants.
    
    Change-Id: I8dceca72a1ba80fc7b1830c5ab285d444f530457
    
    build: Set QCOM variants for non-QCOM_HARDWARE defined targets
    
    * Nexus devices and others typically do not define the QCOM_HARDWARE
      flag, so the variant path should always default to the AOSP variant.
    * Unconditionally set the variant to the AOSP HAL by default.
    
    Change-Id: I714170897128f92718af266366cfcbf3136e8981
    
    build: Automatically set QCOM audio, display, media variants
    
    * Device platform should determing the HAL that ought to be used. This
      commit forces QCOM_HARDWARE to select the -caf HAL variants, which
      are then broken down by platform within the variant path.
    
    Change-Id: I6fc7a3def7b93112f034a3b89552f302727cdbf8
    
    build: Enable QCOM_BSP, QC_AV with QCOM_HARDWARE
    
    Change-Id: Ibf9dd35272521109fea52e46bacf6e1e3074ed6a
    
    pathmap: Point QC HAL pathmaps directly to the source
    
    Directly map to the actual HAL directory, including the
    board platform. This lets project-path-for point directly
    to the respective HALs.
    
    Change-Id: Ic4ed61bbdea9d0b5683502bf84a8410e76858527
    
    Enable QCOM_BSP_LEGACY flag for pre-8974 targets
    
    Change-Id: I895f1b1cbad0d260e1bca87ccb61e6194de023fd
    
    Fix QCOM_BSP_LEGACY
    
    Checks in the Android.mk files do not enable the cflags for .h
    files, causing a build that crashes constantly.
    
    Change-Id: I315c760488445629fda860ba70066417c7d68b8b
    
    build: Clean up QCOM flag definitions
    
    Change-Id: I66bca2db83260ccd65b82e540ee9f7961f00b030
    
    build: Add QCOM flags to Clang CFLAGS
    
    Change-Id: I56f0d4106f5d3d27c1ace744d30c1c81f0052bbd
    
    build: Set QCOM global cflags for secondary arch
    
    Change-Id: I637d94f9b0ddee2c8d6057de44355e482163dd36
    
    build: Fix copypasta in 2nd target clang flags
    
    Change-Id: Ieedce8754b145e53d72a5d6a4b41fbe5cdf6473f
    
    build: Allow devices to specify a RIL variant
    
    Change-Id: Ia5d35d916be358fb7be7ead6bd05cc3628b35a37
    
    build: qcom: Set the correct project path for device-specific cameras
    
    Change-Id: I37696dae2c05c4e9f4fc824fbba63af47483ea17
    
    build: Add a couple more custom pathmap variables
    
    Change-Id: Ie44b9844c6d82dd73d1ea1f16b565f8bb06652c1
    
    build: Allow QC platforms to share/customize HAL variants
    
    * Allow devices to override HAL variant using QCOM_HARDWARE_VARIANT
    * 8909 and 8916 share HALs by default
    * 8610, 8226, 8974 share HALs by default
    * 8992, 8994 share HALs by default
    
    Change-Id: Idfbdc405a35c5ba518fc71c8b691fc177ddf9068

---
+++ core/qcom_target.mk
@@ -0,0 +1,32 @@
+# Target-specific configuration
+
+# Populate the qcom hardware variants in the project pathmap.
+define qcom-set-path-variant
+$(call project-set-path-variant,qcom-$(2),TARGET_QCOM_$(1)_VARIANT,hardware/qcom/$(2))
+endef
+
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+    qcom_flags := -DQCOM_HARDWARE
+    qcom_flags += -DQCOM_BSP
+
+    TARGET_USES_QCOM_BSP := true
+    TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+
+    ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
+        # Enable legacy graphics functions
+        qcom_flags += -DQCOM_BSP_LEGACY
+    endif
+
+    TARGET_GLOBAL_CFLAGS += $(qcom_flags)
+    TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
+    CLANG_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
+    CLANG_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
+
+$(call project-set-path,qcom-audio,hardware/qcom/audio-caf/$(TARGET_BOARD_PLATFORM))
+$(call project-set-path,qcom-display,hardware/qcom/display-caf/$(TARGET_BOARD_PLATFORM))
+$(call project-set-path,qcom-media,hardware/qcom/media-caf/$(TARGET_BOARD_PLATFORM))
+else
+$(call project-set-path,qcom-audio,hardware/qcom/audio/default)
+$(call project-set-path,qcom-display,hardware/qcom/display/$(TARGET_BOARD_PLATFORM))
+$(call project-set-path,qcom-media,hardware/qcom/media/default)
+endif
